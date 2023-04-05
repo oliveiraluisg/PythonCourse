238a6ef305a89f1ba048dc403f1e4dd97f125d3e
oliveiraluisg <luisg.araujooliveira@gmail.com>
2023-04-05 18:31:09 -0300

    parte2

---
+++ lacos_repeticao.py
@@ -0,0 +1,17 @@
+numsorte = 21
+
+numescolhido = int(input('Advinhe meu número da sorte entre 1 e 30: '))
+
+while numescolhido != numsorte :
+    print('Digite novamente vc errou')
+    numescolhido = int(input('Advinhe meu número da sorte entre 1 e 30: '))
+
+print('Parabéns, você acertou o meu número da sorte!')
+
+#estrutura controlada
+# contador = 0
+
+# while contador < 10:
+#     print(contador)
+
+#     contador = contador + 1
